dd5a4ce55ed3c5f5381093699d10eacce470e39c
Paul Williams <paulswilliams@me.com>
2012-10-27 18:46:34 +0100

    first scenario specced in cucumber

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,11 +1,26 @@
+class Exhibit
+  attr_accessor :title
+end
+ 
+
+Given /^the following exhibits exist within mosi:$/ do |exhibits|
+  exhibits.hashes.each do | exhibit |
+  	new_exhibit = Exhibit.new
+  	new_exhibit.title = exhibit
+
+  end
+end
+
 When /^I start an mositrail$/ do
   visit '/mositrail/start'
 end
 
-Then /^the application will present the first clue$/ do
-  page.should have_content("Let's find your first exhibit")
-  page.should have_content("This one will drive you up the wall")
+Then /^the application will present the clue: (.*)$/ do | clue |
+  page.should have_content(clue)
+	page.should have_content("This one will drive you up the wall")
 end
+
+
 
 And /^the ability to provide an exhibit code$/ do
   have_field("PIN")
