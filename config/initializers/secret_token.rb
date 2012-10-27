a59ca3e89e2156a1ef385bd7c1b58839c818d83b
Paul Williams <paulswilliams@me.com>
2012-10-27 17:09:15 +0100

    intial rails creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mositrails::Application.config.secret_token = 'ae0595c28e98277697dbe156ae48510847e059f0104027dd6b5309e188cad5776acc70bd8681de4d75792ab4639c603782fb204910b0db12783cceac470ea144'
