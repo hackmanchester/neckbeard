415c4ee5bb288b8493f0fd898caf13148fb54fd9
Paul Williams <paulswilliams@me.com>
2012-10-27 17:34:14 +0100

    added Starting an mositrail empty feature

---
+++ features/follow_mositrail.feature
@@ -0,0 +1,6 @@
+Feature: To enable me to participate in a mositrail, as a visitor, I want to follow a set of clues
+
+Scenario: Starting an mositrail  
+  When I start an mositrail
+  Then the application will present the first clue
+  And the ability to provide an exhibit code
---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+When /^I start an mositrail$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the application will present the first clue$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the ability to provide an exhibit code$/ do
+  pending # express the regexp above with the code you wish you had
+end
